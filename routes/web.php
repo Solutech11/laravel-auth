9430338b3dd90331588438dfdb3837c0a4f00dff
Solutech11 <soluwizy@gmail.com>
2025-01-14 22:11:09 +0100

    auth done

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api')->controller(Auth::class)->group(function(){
     Route::post('register','RegisterFunc');
+
+    Route::post('login','LoginFunc');
+
 });
